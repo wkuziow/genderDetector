52e0133fa61ca6287f0032eb4b006049a200c64b
wkuziow <wkuziow@gmail.com>
2021-02-08 23:37:17 +0100

    first commit

---
+++ src/main/java/pl/kuziow/genderdetector/GenderDetectorApplication.java
@@ -0,0 +1,13 @@
+package pl.kuziow.genderdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kuziow/genderdetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kuziow.genderdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
